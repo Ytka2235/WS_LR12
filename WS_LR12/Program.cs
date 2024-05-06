c16818be564c5106e5b0b8d2b4ced5c76c369a8f
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2024-05-06 17:25:37 +0300

    Добавьте файлы проекта.

---
+++ WS_LR12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "qw234.55.7.8eqwweqwq99999.23.99.0 qweqweqwqwwq";
+        Regex regex = new Regex(@"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+        MatchCollection matches = regex.Matches(str);
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+}
